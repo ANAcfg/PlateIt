4733f13cb8888a3ad03be697993d70613b734b32
Ana <anacfg202@gmail.com>
2022-12-10 18:11:05 -0500

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,56 @@
-import React from 'react'
+import React,{useState, useEffect} from "react";
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin, { Draggable } from '@fullcalendar/interaction';
 
 import '../styles/App.css';
 
 
 function App() {
-  const events = [{title: "today's event", date: new Date()}];
+  // const events = [{title: "today's event", date: new Date()}];
+  // let array  =[
+  //     {title:"event 1", id:"1"},
+  //     {title:"event 2", id: "2"}
+  //   ]
+  const [recepies,setRecepies] = useState([
+    {title:"event 1", id:"1"},
+    {title:"event 2", id: "2"}
+  ]);
+  useEffect(()=>{
+    let draggableEl = document.getElementById("draggable");
+    new Draggable(draggableEl, {
+      itemSelector: ".fc-event",
+      eventData: function (eventEl){
+        let title = eventEl.getAttribute("title");
+        let id = eventEl.getAttribute("data");
+        return{
+          title:title,
+          id:id
+        };
+      }
+    })
+    
+
+
+  });
   return (
     <div className="App">
       <h1 >
        Plate it!
       </h1>
       <div>
-        <div>
-          search recipies plans here!
+        <div id = "draggable">
+          {recepies.map((event) => (
+            <div 
+              className="fc-event" 
+              title={event.title}
+              data = {event.id}
+              key = {event.id}
+              >
+              {event.title}
+            </div>
+          ))
+          }
         </div>
 
         <FullCalendar 
@@ -24,8 +60,7 @@ function App() {
             center: "title",
             right: "timeGridWeek,timeGridDay"
           }}
-          plugins={[timeGridPlugin]}
-          events={events}
+          plugins={[timeGridPlugin,interactionPlugin]}
           />
       </div>
     </div>
