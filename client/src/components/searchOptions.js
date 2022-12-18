deab0ea2420127c9376f4fe5018c03a31391ec08
Ana <anacfg202@gmail.com>
2022-12-18 15:46:20 -0500

    remove extra files

--- client/src/components/searchOptions.js
+++ client/src/components/searchOptions.js
@@ -1,6 +1,5 @@
 
 const searchOptions = [
-    //{ label: 'Search Recipes By Nutrients', value: 'Nutrients' },
     { label: 'Get Random Recipes', value: 'RandomRecipes'},
     { label: 'Get Similar Recipes', value: 'SimilarRecipes'},
     { label: 'Search Recipes by Name', value: 'SearchRecipes'}
